97edf0fbc4d2218469e4c1f45e1db838ddc873bb
Akash <122150350+akashm6@users.noreply.github.com>
2024-08-05 21:33:00 -0500

    3 new page routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/login";
 import NumArtistsPage from "./pages/num_artists";
+import TopArtistsPage from "./pages/topartists";
+import AboutPage from "./pages/about";
+import ConcertInfoPage from "./pages/concertinfopage";
 
 function App() {
 
@@ -10,6 +13,9 @@ function App() {
     <Routes>
       <Route path = "/" element = {<LoginPage/>} />
       <Route path = '/num_artists' element = {<NumArtistsPage/>} />
+      <Route path = '/top_artists' element = {<TopArtistsPage/>} />
+      <Route path = '/about' element = {<AboutPage/>} />
+      <Route path = '/concerts/:artistName' element = {<ConcertInfoPage/>} />
     </Routes>
     </BrowserRouter>
   )
