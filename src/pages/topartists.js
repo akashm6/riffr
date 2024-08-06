1ecfe90b9b95649139ecf1e193c9daf0fd5bc653
Akash <122150350+akashm6@users.noreply.github.com>
2024-08-05 21:33:59 -0500

    sends user input to backend

---
+++ src/pages/topartists.js
@@ -0,0 +1,38 @@
+// src/components/TopArtistsPage.js
+import React, { useEffect, useState } from "react";
+import ArtistCard from "../components/artistcard";
+import '../components/top_artists.css';
+import { useLocation } from "react-router-dom"; 
+
+function TopArtistsPage() {
+    const [artists, setArtists] = useState([]);
+    const location = useLocation();
+
+    const numArtists = location.state?.numArtists || 10; 
+
+    useEffect(() => {
+        const fetchArtists = async () => {
+            const response = await fetch(`http://localhost:5000/top_artists?num_artists=${numArtists}`, {
+                credentials: 'include'
+            });
+            if (!response.ok) {
+                console.error("Failed to fetch artists");
+                return;
+            }
+            const data = await response.json();
+            setArtists(data);
+        };
+
+        fetchArtists();
+    }, [numArtists]);
+
+    return (
+        <div className="top-artists-container">
+            {artists.map((artist, index) => (
+                <ArtistCard key={index} artist={artist} />
+            ))}
+        </div>
+    );
+}
+
+export default TopArtistsPage;
