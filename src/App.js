371dbc79640810f287ddb75d9fcc473986a6b6fd
Akash <122150350+akashm6@users.noreply.github.com>
2024-08-28 15:46:19 -0500

    added available country path

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NumArtistsPage from "./pages/num_artists";
 import TopArtistsPage from "./pages/topartists";
 import AboutPage from "./pages/about";
 import ConcertInfoPage from "./pages/concertinfopage";
+import AvailableCountriesPage from "./pages/countrypage";
 
 function App() {
 
@@ -15,7 +16,8 @@ function App() {
       <Route path = '/num_artists' element = {<NumArtistsPage/>} />
       <Route path = '/top_artists' element = {<TopArtistsPage/>} />
       <Route path = '/about' element = {<AboutPage/>} />
-      <Route path = '/concerts/:artistName' element = {<ConcertInfoPage/>} />
+      <Route path = '/concerts/:artistName/:country' element = {<ConcertInfoPage/>} />
+      <Route path = "/available-countries/:artistName" element = {<AvailableCountriesPage/>} />
     </Routes>
     </BrowserRouter>
   )
