6ceb27877736e1a710f9091b4d94eb998587cbdd
Akash <122150350+akashm6@users.noreply.github.com>
2024-08-28 15:44:04 -0500

    fetches eligible countries + ticketmaster api + date parsing

--- api.py
+++ api.py
@@ -1,18 +1,19 @@
 from flask import Flask, request, redirect, session, jsonify
-from flask_cors import CORS
 from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+from datetime import datetime
+from flask_cors import CORS
+import requests
 import spotipy
 import pathlib
+import csv
 import os
-from dotenv import load_dotenv
-import requests
-from datetime import datetime
- 
+
 path = pathlib.Path('src') / '.env'
 load_dotenv(dotenv_path=path)
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True) 
+CORS(app, supports_credentials=True)
 app.secret_key = os.environ.get('SECRET_KEY')
 
 sp_oauth = SpotifyOAuth(
@@ -22,7 +23,7 @@ sp_oauth = SpotifyOAuth(
     scope="user-library-read user-top-read user-read-playback-state user-read-currently-playing user-read-recently-played"
 )
 
-predict_hq_token = os.environ.get('PREDICT_HQ_API_TOKEN')
+ticketmaster_token = os.environ.get('TICKETMASTER_CONSUMER_KEY')
 
 @app.route('/')
 def login():
@@ -61,47 +62,63 @@ def fetch_top_artists():
 
 @app.route('/concerts', methods=['GET'])
 def fetch_concerts():
-    artist_name = request.args.get('artist_name')
+    artist_name = request.args.get('artistName')
+    country_code = request.args.get('country')
     if not artist_name:
         return jsonify({'error': 'Artist name is required'}), 400
 
-    url = f'https://api.predicthq.com/v1/events/?q={artist_name}&category=concerts'
-    headers = {
-        'Authorization': f'Bearer {predict_hq_token}',
-    }
+    url = f"https://app.ticketmaster.com/discovery/v2/events.json?size=10&keyword={artist_name}&countryCode={country_code}&apikey={ticketmaster_token}"
 
-   
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
     response.raise_for_status()
     data = response.json()
+    data = data['_embedded']['events']
     results = []
     concert_title = ''
-    for item in data['results']:
-        for title in item['alternate_titles']:
-            if 'tour' in title.lower():
-                concert_title = title
-        concert_title = format_title(concert_title) if concert_title else format_title(item['alternate_titles'][0])
+    for event in data:
+        #print(event)
+        ticket_url = event['url']
+        name = event['name']
+        date = str(parse_date(event['dates']['start']['localDate']))
+        date = date[:date.index(' ')]
+        time = event['dates']['start']['localTime']
+        venue = event['_embedded']['venues'][0]['name']
+        city = event['_embedded']['venues'][0]['city']['name']
+        address = f"{event['_embedded']['venues'][0]['address']['line1']}, {city},{country_code}"
         results.append({
-            'title': concert_title,
-            'venue_name': item['entities'][0]['name'],
-            'address': item['entities'][0]['formatted_address'],
-            'date': parse_date(item['start_local'])
+            'title': name,
+            'artist_name': artist_name,
+            'venue_name': venue,
+            'address': address,
+            'city': city,
+            'date': date,
+            'time': time,
+            'ticket_link': ticket_url
         })
-
     return jsonify(results)
+
+@app.route('/available-countries', methods = ['GET'])
+def get_available_countries():
+    keyword = request.args.get('artistName')
+    available = []
+    with open('./country_codes.csv', newline='') as file:
+        reader = csv.DictReader(file)
+        country_data = [row for row in reader]
+    for country in country_data:
+        url = f"https://app.ticketmaster.com/discovery/v2/events.json?size=10&keyword={keyword}&countryCode={country['country_code']}&apikey={ticketmaster_token}"
+        try:
+            response = requests.get(url)
+            data = response.json()
+            ticket_link = data['_embedded']
+            available.append(country['country_code'])
+        except:
+            continue
+    return jsonify(available)
 
 def parse_date(date):
     date = datetime.fromisoformat(date.replace("Z", "+00:00"))
     formatted_date = date.strftime("%m/%d/%Y %I:%M %p")
     return formatted_date
 
-def format_title(title):
-    formatted = ''
-    for letter in title:
-        if letter.isalnum() or letter == ' ':
-            formatted += letter
-    return formatted
-
 if __name__ == '__main__':
     app.run(debug=True)
-
