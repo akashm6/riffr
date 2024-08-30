059b4ce4213efd29f54f988345eabfc5fa0b4946
Akash <122150350+akashm6@users.noreply.github.com>
2024-08-29 20:33:51 -0500

    google maps integration

--- src/components/concertcard.js
+++ src/components/concertcard.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import '../components/concertcard.css';
 
 function ConcertCard({ concert }) {
+    const [isMapVisible, setMapVisible] = useState(false);
+
+    const handleMapToggle = () => {
+        setMapVisible(!isMapVisible);
+    };
+
+    const getMapUrl = () => {
+        const address = encodeURIComponent(concert.address);
+        const maps_key = encodeURIComponent(concert.maps_key)
+        return `https://www.google.com/maps/embed/v1/place?key=${maps_key}&q=${address}`;
+    };
 
     return (
         <div className="concert-card">
@@ -11,7 +22,24 @@ function ConcertCard({ concert }) {
             <p><strong>Date:</strong> {concert.date}</p> 
             <p><strong>City:</strong> {concert.city}</p> 
             <p><strong>Time:</strong> {concert.time}</p> 
-            <a href={concert.ticket_link} target="_blank" rel="noopener noreferrer" className="buy-more-link">Buy Tickets</a>
+            <a href={concert.ticket_link} target="_blank" rel="noopener noreferrer" className="buy-ticket-button">Buy Tickets</a>
+            <button onClick={handleMapToggle} className="show-map-button">
+                {isMapVisible ? "Hide Map" : "Show Map"}
+            </button>
+
+            {isMapVisible && (
+                <div className="map-popup">
+                    <iframe className = 'map-frame'
+                        title="Concert Venue Map"
+                        src={getMapUrl()}
+                        width="600"
+                        height="450"
+                        style={{ border: 0 }}
+                        allowFullScreen=""
+                        loading="lazy"
+                    ></iframe>
+                </div>
+            )}
         </div>
     );
 }
