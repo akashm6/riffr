85dfe518169c5f87172ad46eda20a2bb01120e8a
Akash <122150350+akashm6@users.noreply.github.com>
2024-09-17 23:39:52 -0500

    added back button

--- src/pages/topartists.js
+++ src/pages/topartists.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from "react";
 import ArtistCard from "../components/artistcard";
 import '../components/top_artists.css';
-import { useLocation } from "react-router-dom"; 
+import { useLocation, useNavigate } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 
 function TopArtistsPage() {
     const [artists, setArtists] = useState([]);
     const location = useLocation();
+    const navigate = useNavigate();
 
     const numArtists = location.state?.numArtists || 10; 
 
     useEffect(() => {
         const fetchArtists = async () => {
             const response = await fetch(`http://localhost:5000/top_artists?num_artists=${numArtists}`, {
+                grant_type: 'client_credentials',
                 credentials: 'include'
             });
             if (!response.ok) {
@@ -30,6 +33,7 @@ function TopArtistsPage() {
             {artists.map((artist, index) => (
                 <ArtistCard key={index} artist={artist} />
             ))}
+            <Link className = 'back-to-home-button' onClick = {() => navigate(-1)}>Go Back</Link>
         </div>
     );
 }
