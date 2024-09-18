5da4562a3c2f91f2fd2d0dc18b8335fc05238eb5
Akash <122150350+akashm6@users.noreply.github.com>
2024-09-17 23:44:20 -0500

    removes redundant handlers

--- src/components/artistcard.js
+++ src/components/artistcard.js
@@ -3,27 +3,10 @@ import { useNavigate } from "react-router-dom";
 import './artistcard.css';
 
 function ArtistCard({ artist }) {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [listeningStats, setListeningStats] = useState(null);
     const navigate = useNavigate(); 
 
-    const handleStatsClick = async () => {
-        
-        const response = await fetch(`http://localhost:5000/listening_stats?artist=${artist.name}`, {
-            credentials: 'include',
-        });
-        const data = await response.json();
-        setListeningStats(data);
-        setIsModalOpen(true);
-    };
-
-    const handleCloseModal = () => {
-        setIsModalOpen(false);
-        setListeningStats(null);
-    };
-
     const handleConcertInfoClick = () => {
-        navigate(`/concerts/${encodeURIComponent(artist.name)}`); 
+        navigate(`/available-countries/${encodeURIComponent(artist.name)}`); 
     };
 
     return (
