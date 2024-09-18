c3cf0a395409b0a21085954e7183bd21275000b9
Akash <122150350+akashm6@users.noreply.github.com>
2024-09-17 23:40:11 -0500

    about page to show everyone how cool i am

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,27 +5,30 @@ import { Link } from "react-router-dom";
 function AboutPage() {
     return (
         <div className="about-container">
-            <h1 className="about-title">What's Riff Notif?</h1>
+            <h1 className="about-title">what's riffr?</h1>
             <p className="about-description">
-                Riff Notif makes keeping up with your favorite musicians easier and more centralized
-                than ever. Concert finding can be a hassle, so Riff Notif strives to make it as simple 
+                riffr makes keeping up with your favorite musicians easier and more centralized
+                than ever. Concert finding can be a hassle, so riffr strives to make it as simple 
                 as possible.
             </p>
             <p className="about-description">
-                Sign in with Spotify, type in a number, and see concert information for all of your
-                top listened artists, all in one place. 
+                Sign in with Spotify, type in a number, and view concert information for all of your
+                top listened artists, in every possible country, all in one place. 
             </p>
             <p className="about-description">
-                With up-to-date venue locations, concert times, and even Google Map support, 
-                Riff Notif is the gateway for a simple, streamlined way to get tickets to your
-                favorite concerts.
+                Find ticket links, up-to-date venue locations, concert times, and more. 
+                Unsure about the surrounding area of the venue? Explore satellite views of the area.
+            </p>
+            <p className = 'about-description'>
+                With riffr, you can experience a new level of simplicity, designed to get you tickets
+                to your favorite concerts in the least clicks possible.
             </p>
             <p className="about-description">
                 My name is Akash Mohan, and I'm currently a Computer Science and Data Science student
                 at the University of Wisconsin-Madison. Here's some other <a href="https://github.com/akashm6" target="_blank" rel="noopener noreferrer">things</a> I've worked on.
             </p>
 
-            <Link to = '/' className="back-to-home-button">Back to Home</Link>
+            <Link to = '../' className="back-to-home-button">go back</Link>
 
         </div>
     );
