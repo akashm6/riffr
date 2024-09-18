aae32260a8aeff44b318f9f1f53e5f208ff9a787
Akash <122150350+akashm6@users.noreply.github.com>
2024-09-17 23:38:07 -0500

    handles user input edge cases

--- src/pages/num_artists.js
+++ src/pages/num_artists.js
@@ -3,11 +3,17 @@ import { useNavigate } from "react-router-dom";
 import '../components/num_artists.css';
 
 function NumArtistsPage() {
-    const [numArtists, setNumArtists] = useState(0);
+    const [numArtists, setNumArtists] = useState("");
+    const [error, setError] = useState("");
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        if (numArtists > 50) {
+            setError("Please enter a number less than or equal to 50.");
+            return;
+        }
+        setError(""); 
         try {
             const response = await fetch(`http://localhost:5000/top_artists?num_artists=${numArtists}`, {
                 credentials: 'include',
@@ -33,10 +39,13 @@ function NumArtistsPage() {
                         name="number_of_artists" 
                         value={numArtists} 
                         onChange={(e) => setNumArtists(e.target.value)}
+                        min="1" 
+                        max="50"
                     />
                 </label>
                 <input className='artist-submit-box' type="submit" value="Submit" />
             </form>
+            {error && <div className="error-box">{error}</div>}
             <footer className="footer">
                 Created by <a href="https://github.com/akashm6" target="_blank" rel="noopener noreferrer">Akash Mohan</a>
             </footer>
